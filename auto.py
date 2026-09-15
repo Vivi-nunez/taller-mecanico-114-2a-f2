57ca1fc6dabc5e1d90fbea13e65bc0a388cb9e27
Vivi-nunez <vivi.nvera27@gmail.com>
2026-09-14 22:00:16 -0300

    Implementa metodo tarifa_hora retornando 30000 en clase Auto

--- auto.py
+++ auto.py
@@ -3,5 +3,6 @@ from vehiculo import Vehiculo
 
 # Define la clase Auto que hereda de la clase base Vehiculo
 class Auto(Vehiculo):
-    # Declaración pass indica que por ahora la clase no añade métodos o atributos adicionales
-    pass
+    # Método para retornar la tarifa por hora específica para un auto
+    def tarifa_hora(self) -> int:
+        return 30000
